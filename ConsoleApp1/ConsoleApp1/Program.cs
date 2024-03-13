3d034a7cb025d55a535df18895a034f0b60b62df
MykolaKriuk <mykolakryukov@gmail.com>
2024-03-13 20:49:25 +0100

    Added c# files

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, User!!! YO!");
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(GetAvg(arr));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+
+    return (double)sum / arr.Length;
+}
